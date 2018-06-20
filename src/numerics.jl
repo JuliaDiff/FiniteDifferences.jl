e5d3471b7d0646ae7f0a95f1672f2d386e4ba74e
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2018-06-20 16:45:23 +0100

    Improve test for approximate equality

--- src/numerics.jl
+++ src/numerics.jl
@@ -5,10 +5,11 @@ export assert_approx_equal
 
 Assert that `x` is approximately equal to `y`.
 
-Let `ε_z = ε_abs / ε_rel`. Call `x` and `y` small if `abs(x) + abs(y) < ε_z`, and call `x`
-and `y` large otherwise. If this assertion succeeds, then it is guaranteed that
-`abs(x - y) < 2ε_rel * (abs(x) + abs(y))` if `x` and `y` are large, and
-`abs(x - y) < 2ε_abs` if `x` and `y` are small.
+Let `eps_z = eps_abs / eps_rel`. Call `x` and `y` small if
+`abs(x) < eps_z` and `abs(y) < eps_z`, and call `x` and `y` large otherwise.  If this
+function returns `True`, then it is guaranteed that
+`abs(x - y) < 2 eps_rel max(abs(x), abs(y))` if `x` and `y` are large, and
+`abs(x - y) < 2 eps_abs` if `x` and `y` are small.
 
 # Arguments
 - `x`: First object to compare.
@@ -18,9 +19,9 @@ and `y` large otherwise. If this assertion succeeds, then it is guaranteed that
 - `desc`: Description of the comparison. Omit or set to `false` to have no description.
 """
 function assert_approx_equal(x, y, ε_abs, ε_rel, desc)
-    if abs(x - y) >= ε_abs + ε_rel * (abs(x) + abs(y))
+    if abs(x - y) >= ε_abs + ε_rel * max(abs(x), abs(y))
         msg = "$(desc != false ? "\"$desc\": " : "")large deviation from reference:\n" *
-              "  relative error:  $(@sprintf "%.3e" abs(x - y) / (abs(x) + abs(y)))\n" *
+              "  relative error:  $(@sprintf "%.3e" abs(x - y) / max(abs(x), abs(y)))\n" *
               "    tolerance:     $(@sprintf "%.3e" ε_rel)\n" *
               "  absolute error:  $(@sprintf "%.3e" abs(x - y))\n" *
               "    tolerance:     $(@sprintf "%.3e" ε_abs)\n"
