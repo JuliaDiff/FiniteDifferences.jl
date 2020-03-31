c02623a0d80e39893a6d85c67073a865b7bad382
willtebbutt <wt0881@my.bristol.ac.uk>
2020-03-31 15:31:03 +0100

    Tidies up docstring

--- src/to_vec.jl
+++ src/to_vec.jl
@@ -1,7 +1,8 @@
 """
     to_vec(x)
 
-Transform `x` into a `Vector`, and return the vector, and a closure which inverts the transformation.
+Transform `x` into a `Vector`, and return the vector, and a closure which inverts the
+transformation.
 """
 function to_vec(x::Number)
     function Number_from_vec(x_vec)
