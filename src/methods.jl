16ef6909ae77ee7a30c249029a7b42c3da98e5cb
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-01-02 14:22:27 +0100

    Make grids tuples

--- src/methods.jl
+++ src/methods.jl
@@ -385,19 +385,21 @@ function _make_adaptive_bound_estimator(
     end
 end
 
-_forward_grid(p::Int) = collect(0:(p - 1))
+_forward_grid(p::Int) = Tuple(0:(p - 1))
 
-_backward_grid(p::Int) = collect((1 - p):0)
+_backward_grid(p::Int) = Tuple((1 - p):0)
 
 function _central_grid(p::Int)
     if isodd(p)
-        return collect(div(1 - p, 2):div(p - 1, 2))
+        return Tuple(div(1 - p, 2):div(p - 1, 2))
     else
-        return vcat(div(-p, 2):-1, 1:div(p, 2))
+        return ((div(-p, 2):-1)..., (1:div(p, 2))...)
     end
 end
 
-_exponentiate_grid(grid::Vector, base::Int=3) = sign.(grid) .* base .^ abs.(grid) ./ base
+function _exponentiate_grid(grid::Tuple{Vararg{Int}}, base::Int=3)
+    return sign.(grid) .* div.(base .^ abs.(grid), base)
+end
 
 function _is_symmetric(vec::Vector; centre_zero::Bool=false, negate_half::Bool=false)
     half_sign = negate_half ? -1 : 1
