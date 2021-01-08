9bcb2d23b52a9ac074d06c2537f3de6d22518ec6
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-01-08 15:40:45 +0100

    Automatically update index.md in docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,14 @@
 using Documenter, FiniteDifferences
 
+# Copy the README.
+repo_root = dirname(dirname(@__FILE__))
+cp(
+    joinpath(repo_root, "README.md"),
+    joinpath(repo_root, "docs", "src", "index.md");
+    # `index.md` will already exist, so we set `force=true` to overwrite it.
+    force=true
+)
+
 makedocs(
     modules=[FiniteDifferences],
     format=Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true"),
