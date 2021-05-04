f46293a89c8dcfb5c9d82ead15690b80f3608199
Miha Zgubic <mzgubic@users.noreply.github.com>
2021-05-04 11:36:19 +0100

    copy `rand_tangent(::BigFloat)` from `ChainRulesTestUtils` (#155)

--- src/rand_tangent.jl
+++ src/rand_tangent.jl
@@ -13,6 +13,10 @@ rand_tangent(rng::AbstractRNG, x::Integer) = DoesNotExist()
 
 rand_tangent(rng::AbstractRNG, x::T) where {T<:Number} = randn(rng, T)
 
+# TODO: right now Julia don't allow `randn(rng, BigFloat)` 
+# see: https://github.com/JuliaLang/julia/issues/17629
+rand_tangent(rng::AbstractRNG, ::BigFloat) = big(randn(rng))
+
 rand_tangent(rng::AbstractRNG, x::StridedArray) = rand_tangent.(Ref(rng), x)
 
 function rand_tangent(rng::AbstractRNG, x::T) where {T<:Tuple}
