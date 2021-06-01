f55f885ce81d4012b132030709d45abafe5fdb82
Lyndon White <oxinabox@ucc.asn.au>
2021-06-01 14:39:57 +0100

    Relax big further

--- test/rand_tangent.jl
+++ test/rand_tangent.jl
@@ -93,7 +93,7 @@ using FiniteDifferences: rand_tangent
             @test length(string(rand_tangent(1.0))) <= 6
             @test length(string(rand_tangent(1.0 + 1.0im))) <= 12
             @test length(string(rand_tangent(1f0))) <= 12
-            @test length(string(rand_tangent(big"1.0"))) <= 9
+            @test length(string(rand_tangent(big"1.0"))) <= 12
         end
     end
 end
