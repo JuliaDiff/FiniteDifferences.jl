09c6767bc66c998ede8b4491b3859719d7731499
William S. Moses <gh@wsmoses.com>
2025-09-12 17:39:04 -0500

    fix

--- test/grad.jl
+++ test/grad.jl
@@ -235,8 +235,8 @@ end
 @testset "jvp: Estimate step correctly for when some terms are nan/infinite" begin
     fdm = FiniteDifferences.central_fdm(5, 1)
     res = jvp(fdm, partial_nan_returning, (3.1, 2.7))
-    @test res[1] ≈ 2.7
+    @test res[2] ≈ 2.7
 
     res = jvp(fdm, partial_nondet_returning, (3.1, 2.7))
-    @test res[1] ≈ 2.7
+    @test res[2] ≈ 2.7
 end
